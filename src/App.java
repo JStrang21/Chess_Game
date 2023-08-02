6b2ad316af69119f76f0a9385ad10c84027ba6be
Justin Strang <Justinstrang100@gmail.com>
2023-08-02 15:44:35 -0400

    Rook seems to be working: doesn't move when other pieces in the way, only moves up and down

--- src/App.java
+++ src/App.java
@@ -19,20 +19,32 @@ public class App {
         //Rook tests
         Piece bRook = new Rook(2);
         Piece wRook = new Rook(1);
+        Piece wPawn = new Pawn(1);
+        Piece bPawn = new Pawn(2);
 
         g.board.board[2][0].addPiece(wRook);
         wRook.setCoords(2,0);
         g.board.board[2][7].addPiece(bRook);
         bRook.setCoords(2,7);
+        g.board.board[2][1].addPiece(wPawn);
+        wPawn.setCoords(2,1);
+        /*g.board.board[2][6].addPiece(bPawn);
+        bPawn.setCoords(2,6);*/
 
         //Before move
         Piece rookOne = g.board.getPiece(2, 0);
         Piece rookTwo = g.board.getPiece(2, 7);
+        //Piece wPawn = g.board.getPiece(2, 3);
+        //Piece pTwo = g.board.getPiece(2, 4);
 
-        System.out.println(rookOne.getX() + " " + rookOne.getY() + " " + rookOne.getColor() + " " + rookOne.getName());
-        System.out.println(rookTwo.getX() + " " + rookTwo.getY() + " " + rookTwo.getColor() + " " + rookTwo.getName());
 
-        g.board.movePiece(2, 0, 2, 7);
+        //Left to right
+        //g.board.movePiece(2, 0, 2, 7);
+        //Top to bottom
+        //g.board.movePiece(2,0,5,0);
+        //Left to right with piece inbetween
+        g.board.movePiece(2,7,3,0);
+
 
         //After move
         System.out.println(rookOne.getX() + " " + rookOne.getY() + " " + rookOne.getColor() + " " + rookOne.getName());
